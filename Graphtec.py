b5c388a93ec4bafcd6b5e018ce4b65e34e2fa8dd
Juergen Weigert <jw@suse.de>
2013-05-09 22:50:42 +0200

    setup procedure added, Hard coded american media types added. Attempted to also provide European grammage. This needs checking.

--- Graphtec.py
+++ Graphtec.py
@@ -5,8 +5,42 @@
 import usb.core
 import time
     
-# taken from robocut/Plotter.h:53 ff
+# taken from 
+#  robocut/CutDialog.ui
+#  robocut/CutDialog.cpp
 
+MEDIA = [
+# media, pressure, speed, name
+  ( 100,   27,     10,  "Card without Craft Paper Backing"),
+  ( 101,   27,     10,  "Card with Craft Paper Backing"),
+  ( 102,   10,     10,  "Vinyl Sticker"),
+  ( 106,   14,     10,  "Film Labels"),
+  ( 111,   27,     10,  "Thick Media"),
+  ( 112,    2,     10,  "Thin Media"),
+  ( 113,   10,     10,  "Pen"),
+  ( 120,   30,     10,  "Bond Paper 13-28 lbs (105g)"),
+  ( 121,   30,     10,  "Bristol Paper 57-67 lbs (145g)"),
+  ( 122,   30,     10,  "Cardstock 40-60 lbs (90g)"),
+  ( 123,   30,     10,  "Cover 40-60 lbs (170g)"),
+  ( 124,    1,     10,  "Film, Double Matte Translucent"),
+  ( 125,    1,     10,  "Film, Vinyl With Adhesive Back"),
+  ( 126,    1,     10,  "Film, Window With Kling Adhesive"),
+  ( 127,   30,     10,  "Index 90 lbs (165g)"),
+  ( 128,   20,     10,  "Inkjet Photo Paper 28-44 lbs (70g)"),
+  ( 129,   27,     10,  "Inkjet Photo Paper 45-75 lbs (110g)"),
+  ( 130,   30,      3,  "Magnetic Sheet"),
+  ( 131,   30,     10,  "Offset 24-60 lbs (90g)"),
+  ( 132,    5,     10,  "Print Paper Light Weight"),
+  ( 133,   25,     10,  "Print Paper Medium Weight"),
+  ( 134,   20,     10,  "Sticker Sheet"),
+  ( 135,   20,     10,  "Tag 100 lbs (275g)"),
+  ( 136,   30,     10,  "Text Paper 24-70 lbs (105g)"),
+  ( 137,   30,     10,  "Vellum Bristol 57-67 lbs (145g)"),
+  ( 138,   30,     10,  "Writing Paper 24-70 lbs (105g)"),
+  ( 300, None,   None,  "Custom"),
+]
+
+#  robocut/Plotter.h:53 ff
 VENDOR_ID_GRAPHTEC = 0x0b4d 
 PRODUCT_ID_CC200_20 = 0x110a
 PRODUCT_ID_CC300_20 = 0x111a
@@ -47,19 +81,19 @@ class SilhouetteCameo:
     data = s.dev.read(endpoint, size, timeout=timeout, interface=0) 
     if data is None:
       raise ValueError('read failed: none')
-    return data 
+    return data.tostring()
 
   def status(s):
     # Status request.
     s.write("\x1b\x05")
-    resp = s.read(timeout=5000).tostring()
+    resp = s.read(timeout=5000)
     if len(resp) != 2:    raise ValueError('status response not 2 bytes: %s' % (resp))
     if resp[1] != '\x03': raise ValueError('status response not terminated with 0x03: %s' % (resp[1]))
     if resp[0] == '0': return "ready"
     if resp[0] == '1': return "moving"
     if resp[0] == '2': return "unloaded"
     return resp[0]
-
+  
   def initialize(s):
     # taken from robocut/Plotter.cpp:331 ff
     # Initialise plotter.
@@ -73,3 +107,68 @@ class SilhouetteCameo:
       time.sleep(5.0)
     return st
     
+  def home(s):
+    s.write("TT\x03")
+
+  def get_version(s):
+    s.write("FG\x03")
+    resp = s.read(timeout=10000) # Large timeout because the plotter moves.
+    return resp[0:-2]   # chop of 0x03
+
+  def setup(s, media=132, speed=None, pressure=None, pen=None, trackenhancing=False, landscape=False):
+    """media range is [100..300], default 132, "Print Paper Light Weight"
+       speed range is [1..10], default None, from paper (132 -> 10)
+       pressure range is [1..33], default None, from paper (132 -> 5)
+       pen: True or False, default None (media dependant)
+       trackenhancing: True or False, default False
+       landscape: True or False, default False
+    """
+
+    if media is not None: 
+      if media < 100 or media > 300: media = 300
+      s.write("FW%d\x03" % media);
+      if pen is None: 
+        if media == 113:
+          pen = True
+        else:
+          pen = False
+      for i in MEDIA:
+        if i[0] == media: print "Media=%d, name='%s'" % (media, i[3])
+    if speed is not None: 
+      if speed < 1: speed = 1
+      if speed > 10: speed = 10
+      s.write("!%d\x03" % speed);
+    if pressure is not None: 
+      s.write("FX%d\x03" % pressure);
+
+    # robocut/Plotter.cpp:393 says:
+    # // I think this sets the distance from the position of the plotter
+    # // head to the actual cutting point, maybe in 0.1 mms (todo: Measure blade).
+    # // It is 0 for the pen, 18 for cutting.
+    # // C possible stands for curvature. Not that any of the other letters make sense...
+    cutter = 18
+    if pen: cutter = 0
+    s.write("FC%d\x03" % cutter)
+
+    if trackenhancing is not None: 
+      if trackenhancing: 
+        s.write("FY1\x03")
+      else:
+        s.write("FY0\x03")
+
+    if landscape is not None: 
+      if landscape: 
+        s.write("FN1\x03")
+      else:
+        s.write("FN0\x03")
+
+    # // No idea what this does.
+    s.write("FE0\x03")
+
+    # // Again, no idea. Maybe something to do with registration marks?
+    s.write("TB71\x03")
+    resp = s.read(timeout=10000)        # // Allow 10s. Seems reasonable.
+    if resp != "    0,    0\x03":
+      raise ValueError("setup: Invalid response from plotter.")
+
+
--- arrow_test.py
+++ arrow_test.py
@@ -9,51 +9,10 @@ from Graphtec import SilhouetteCameo
 
 # coordinates in mm, origin int top lefthand corner
 arrow1 = [ (1,6), (21,6), (18,1), (31,11), (18,21), (21,16), (1,16), (4,11), (1,6) ]
+
 dev = SilhouetteCameo()
 state = dev.initialize()
 print state
+print "device version: '%s'" % dev.get_version()
 
-# 
-# if (resp != "0\x03") // 0 = Ready. 1 = Moving. 2 = Nothing loaded. "  " = ??
-# {
-# if (resp == "1\x03")
-#   e = Error("Moving, please try again.");
-# else if (resp == "2\x03")
-#   e = Error("Empty tray, please load media.");	// Silhouette Cameo
-# else
-#   e = Error("Invalid response from plotter: " + resp);
-# goto error;
-# }
-# 
-# // Home the cutter.
-# e = UsbSend(handle, "TT\x03");
-# if (!e) goto error;
-# 
-# // Query version.
-# e = UsbSend(handle, "FG\x03");
-# if (!e) goto error;
-# 
-# // Receive the firmware version.
-# e = UsbReceive(handle, resp, 10000); // Large timeout because the plotter moves.
-# if (!e) goto error;
-# 
-# 
-# e = UsbSend(handle, "FW" + ItoS(media) + "\x03");
-# if (!e) goto error;
-# 
-# e = UsbSend(handle, "!" + ItoS(speed) + "\x03");
-# if (!e) goto error;
-# 
-# e = UsbSend(handle, "FX" + ItoS(pressure) + "\x03");
-# if (!e) goto error;
-# 
-# 
-# e = UsbSend(handle, "FW" + ItoS(media) + "\x03");
-# if (!e) goto error;
-# 
-# e = UsbSend(handle, "!" + ItoS(speed) + "\x03");
-# if (!e) goto error;
-# 
-# e = UsbSend(handle, "FX" + ItoS(pressure) + "\x03");
-# if (!e) goto error;
-# 
+dev.setup(media=132)
