fea403d34ef9cbb3b52fc498fa918cc1bfdb01e6
t0b3 <thomas.bettler@gmail.com>
2020-05-04 21:20:59 +0200

    fix py3 warning on win
    Signed-off-by: t0b3 <thomas.bettler@gmail.com>

--- sendto_silhouette.py
+++ sendto_silhouette.py
@@ -102,7 +102,7 @@ sys.path.append(os.path.dirname(os.path.abspath(sys.argv[0])))
 
 sys_platform = sys.platform.lower()
 if sys_platform.startswith('win'):
-  sys.path.append('C:\Program Files\Inkscape\share\extensions')
+  sys.path.append('C:\\Program Files\\Inkscape\\share\\extensions')
 
 elif sys_platform.startswith('darwin'):
   sys.path.append('~/.config/inkscape/extensions')
