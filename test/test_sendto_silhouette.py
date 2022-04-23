1cbaf5cb579ffeaeec424187d8b5ab87305596f9
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2022-04-23 20:16:56 +0200

    Fix handling of duplicates
    Duplicated nodes had the document transform applied twice.

---
+++ test/test_sendto_silhouette.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+import pytest
+
+from sendto_silhouette import SendtoSilhouette
+
+
+@pytest.fixture
+def data_dir():
+    return Path(__file__).parent / 'data'
+
+
+def test_loading_duplicated_path(data_dir):
+    effect = SendtoSilhouette()
+    effect.parse_arguments([str(data_dir / 'plus_with_duplicate.svg')])
+    effect.load_raw()
+    effect.recursivelyTraverseSvg(effect.document.getroot())
+
+    assert effect.paths == [
+        # First cross
+        [(10.0, 0.0), (10.0, 20.0)],
+        [(0.0, 10), (20.0, 10)],
+        # Second cross, the duplicate
+        [(30.0, 20.0), (30.0, 40.0)],
+        [(20.0, 30), (40.0, 30)],
+    ]
