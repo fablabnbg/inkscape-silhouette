1506778e0aae7cd3ec68d9da94d6a37714b8eb26
brian khuu <mofosyne@gmail.com>
2023-08-22 22:16:06 +1000

    render regmark: points_to_svgd() removed. remove redundant __init__

--- render_silhouette_regmarks.py
+++ render_silhouette_regmarks.py
@@ -40,12 +40,9 @@ REG_SAFE_AREA_MM = 20
 # > The thicker you get, the less accurate registration will be. ~~~ galaxyman47
 REG_MARK_LINE_WIDTH_MM = 0.3
 
-REG_MARK_INFO_FONT_SIZE_PT = 2.5
+REG_MARK_INFO_FONT_SIZE_PX = 2.5
 
 class InsertRegmark(EffectExtension):
-	def __init__(self):
-		EffectExtension.__init__(self)
-
 	def add_arguments(self, pars):
 		# Parse arguments
 		pars.add_argument("-X", "--reg-x", "--regwidth",  type = float, dest = "regwidth",   default = 180.0, help="X mark distance [mm]")
@@ -53,17 +50,6 @@ class InsertRegmark(EffectExtension):
 		pars.add_argument("--rego-x",  "--regoriginx",    type = float, dest = "regoriginx", default = 15.0,  help="X mark origin from left [mm]")
 		pars.add_argument("--rego-y", "--regoriginy",     type = float, dest = "regoriginy", default = 20.0,  help="X mark origin from top [mm]")
 		pars.add_argument("--verbose", dest = "verbose",  type = Boolean, default = False, help="enable log messages")
-
-	#SVG SVGd from (x,y) dimentional points
-	def points_to_svgd(self, p, capped=False):
-		f = p[0]
-		p = p[1:]
-		svgd = "M{:.5f},{:.5f}".format(f[0], f[1])
-		for x in p:
-			svgd += " L{:.5f},{:.5f}".format(x[0], x[1])
-		if capped:
-			svgd += "z"
-		return svgd
 
 	def effect(self):
 		reg_origin_X = self.options.regoriginx
@@ -92,12 +78,12 @@ class InsertRegmark(EffectExtension):
 		# Create horizontal and vertical lines in group for top right corner
 		top_right_x = reg_origin_X+reg_width
 		top_right_path = [(top_right_x-REG_LINE_MM,reg_origin_Y), (top_right_x,reg_origin_Y), (top_right_x,reg_origin_Y + REG_LINE_MM)]
-		regmark_layer.append(PathElement.new(path=self.points_to_svgd(top_right_path), id=REGMARK_TOP_RIGHT_ID, style=f"fill:none; stroke:black; stroke-width:{REG_MARK_LINE_WIDTH_MM}"))
+		regmark_layer.append(PathElement.new(path="M"+str(top_right_path), id=REGMARK_TOP_RIGHT_ID, style=f"fill:none; stroke:black; stroke-width:{REG_MARK_LINE_WIDTH_MM}"))
 
 		# Create horizontal and vertical lines in group for bottom left corner
 		bottom_left_y = reg_origin_Y+reg_length
 		bottom_left_path = [(reg_origin_X+REG_LINE_MM,bottom_left_y), (reg_origin_X,bottom_left_y), (reg_origin_X,bottom_left_y - REG_LINE_MM)]
-		regmark_layer.append(PathElement.new(path=self.points_to_svgd(bottom_left_path), id=REGMARK_BOTTOM_LEFT_ID, style=f"fill:none; stroke:black; stroke-width:{REG_MARK_LINE_WIDTH_MM}"))
+		regmark_layer.append(PathElement.new(path="M"+str(bottom_left_path), id=REGMARK_BOTTOM_LEFT_ID, style=f"fill:none; stroke:black; stroke-width:{REG_MARK_LINE_WIDTH_MM}"))
 
 		# Safe Area Marker #
 		# This draws the safe drawing area
@@ -119,14 +105,14 @@ class InsertRegmark(EffectExtension):
 			(bottom_left_safearea_origin_x,bottom_left_safearea_origin_y),
 			(bottom_left_safearea_origin_x-REG_SAFE_AREA_MM,bottom_left_safearea_origin_y),
 		]
-		regmark_layer.append(PathElement.new(path=self.points_to_svgd(safe_area_points, capped=True), id=REGMARK_SAFE_AREA_ID, style='fill:white;stroke:none'))
+		regmark_layer.append(PathElement.new(path="M"+str(safe_area_points)+"z", id=REGMARK_SAFE_AREA_ID, style='fill:white;stroke:none'))
 
 		# Add some settings reminders to the print layer as a reminder
 		safe_area_note = f"mark distance from document: Left={reg_origin_X}mm, Top={reg_origin_Y}mm; mark to mark distance: X={reg_width}mm, Y={reg_length}mm; "
-		regmark_layer.append(TextElement(safe_area_note, x=f"{(bottom_left_safearea_origin_x+3)}", y=f"{(bottom_left_safearea_origin_y+(REG_SAFE_AREA_MM+reg_origin_Y/2))}", id = REGMARK_NOTES_ID, style=f"font-size:{REG_MARK_INFO_FONT_SIZE_PT}px"))
+		regmark_layer.append(TextElement(safe_area_note, x=f"{(bottom_left_safearea_origin_x+3)}", y=f"{(bottom_left_safearea_origin_y+(REG_SAFE_AREA_MM+reg_origin_Y/2))}", id = REGMARK_NOTES_ID, style=f"font-size:{REG_MARK_INFO_FONT_SIZE_PX}px"))
 
 		# Lock Layer
 		regmark_layer.set_sensitive(False)
-		
+
 if __name__ == '__main__':
 	InsertRegmark().run()
