f3eae44a0594efe5e62538725f619534498a88c3
t0b3 <t0b3@users.noreply.github.com>
2023-08-26 21:02:03 +0200

    test: fix clean_up() to close filehandle (#257)

--- test/test_render_silhouette_regmark.py
+++ test/test_render_silhouette_regmark.py
@@ -36,6 +36,7 @@ class RegmarkTest(InsertRegmarkTest):
         self.e.parse_arguments([self.data_file(self.source_file), "--reglength=300"])
         self.e.load_raw()
         self.e.effect()
+        self.e.clean_up()
 
         """Ensure top-left regmark"""
         self.assertEqual(
