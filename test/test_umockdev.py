3ea6aac40795e673fbcd1578a1863cd677e51828
t0b3 <thomas.bettler@gmail.com>
2025-07-28 08:09:54 +0200

    fix: omit deprecated return value
    closes #342

--- test/test_umockdev.py
+++ test/test_umockdev.py
@@ -13,7 +13,6 @@ class TestUmockdev(unittest.TestCase):
         try:
             result = subprocess.check_output(["umockdev-run", "--device=test/umockdev/cameo3.umockdev", "--ioctl=/dev/bus/usb/003/017=test/umockdev/cameo3.ioctl", sys.executable, "sendto_silhouette.py"], stderr=subprocess.STDOUT)
             print(result.decode())
-            return result
         except subprocess.CalledProcessError as e:
             print(e)
             print(e.output.decode())
